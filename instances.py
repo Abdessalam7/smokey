f72d540d17a119586c929725a6403a152c0be90b
Mohammed Abdessalam BENKAHOUL <34045660+Abdessalam7@users.noreply.github.com>
2026-05-21 12:11:38 +0200

    Add files via upload

--- instances.py
+++ instances.py
@@ -1,24 +1,31 @@
-"""Build the flat list of instances to probe from the ConfigMap JSON."""
+"""Build the flat list of instances to probe from the ConfigMap JSON.
+
+The JSON is the raw airflowctl_inst_list_json variable from Airflow,
+which is a flat array of instance objects.
+"""
 
 
 def build_instances(instances_config, env_list):
     """
-    Walk the ConfigMap and return a flat list of dicts:
+    Walk the airflowctl_inst_list_json array and return a flat list of dicts:
         [{"business_line": ..., "env": ..., "url": ..., "version": ...}, ...]
     Filtered on env_list.
     """
     instances = []
-    for business_line, envs in instances_config.items():
-        for env, json_instances in envs.items():
-            if env not in env_list:
-                continue
-            for ji in json_instances:
-                app_code = ji.get("APP_CODE", "APXXXXXX")
-                release_uid = ji.get("RELEASE_UUID", "e9e9e9e9")
-                instances.append({
-                    "business_line": business_line,
-                    "env": env,
-                    "url": f"astronomer-{app_code}-{env}-{release_uid}",
-                    "version": ji.get("VERSION", "x.y.z"),
-                })
+    for item in instances_config:
+        env = item.get("env", "")
+        if env not in env_list:
+            continue
+
+        # url field already comes as "astronomer-xxx-env-uid.data.cloud.net.intra"
+        # we strip the domain to keep only the slug, matching the existing JSON format
+        full_url = item.get("url", "")
+        url_slug = full_url.replace("https://", "").replace(".data.cloud.net.intra", "")
+
+        instances.append({
+            "business_line": item.get("customer", {}).get("name", "unknown"),
+            "env": env,
+            "url": url_slug,
+            "version": item.get("version", "x.y.z"),
+        })
     return instances
